b1a0670f978dfd09be0b807c40118ae34d719f76
Mai Dung <dungntm@vmogroup.com>
2023-09-11 17:36:22 +0700

    update

--- src/test/java/com/vmo/nopcommerce/features/hompage/ChangeCurrency.java
+++ src/test/java/com/vmo/nopcommerce/features/hompage/ChangeCurrency.java
@@ -5,20 +5,18 @@ import com.vmo.nopcommerce.pageobject.HomePageObject;
 import com.vmo.nopcommerce.pageobject.PageGenerator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class ChangeCurrency extends BaseTest {
-    WebDriver driver;
+    public WebDriver driver;
     private HomePageObject homepage;
-
+    private String browser = "CHROME";
     @BeforeMethod
-    public void setup(String browser) {
+    public void setup(){
         driver = getDriverBrowser(browser);
-        driver.manage().window().maximize();
-        homepage = PageGenerator.getHomePageObject(driver);
+        homepage = new HomePageObject(driver);
     }
+
     @Test
     public void TC06_ChangeCurrency(){
         homepage.gotoURL("https://demo.nopcommerce.com/");
@@ -28,6 +26,6 @@ public class ChangeCurrency extends BaseTest {
     }
     @AfterMethod(alwaysRun = true)
     public void tearDown() {
-        driver.quit();
+        cleanBrowserAndDriver();
     }
 }
